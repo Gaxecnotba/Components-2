82b769e6833b08e51ac3274bb50f06c8b79abc3c
Sebastian Rojas <sebasleprodevj12@gmail.com>
2024-02-28 18:07:51 -0500

    Props Assignment

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,17 @@
 export default function ProductCard(props) {
+  function MouseOver() {
+    console.log("Price", props.price);
+  }
+  function AddtoCartClick() {
+    props.onAddToCart(props.name);
+  }
   return (
     <>
-      <div className="product-card" style={{ border: "1px solid #ccc" }}>
+      <div
+        className="product-card"
+        style={{ border: "1px solid #ccc" }}
+        onMouseOver={MouseOver}
+      >
         <img
           src={
             props.imageUrl || "https://placehold.co/400x200.jpg?text=Product"
@@ -13,7 +23,7 @@ export default function ProductCard(props) {
         <div className="price">${props.price}</div>
         {props.isInStock && <p>Avaible</p>}
         {!props.isInStock && <p>Unvailable</p>}
-        <button>Add to Cart</button>
+        <button onClick={AddtoCartClick}>Add to Cart</button>
       </div>
     </>
   );
--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -1,12 +1,20 @@
 import ProductCard from "./ProductCard";
 
 export default function ProductCardContainer(props) {
+  function EventAddCart(productName) {
+    alert(`You added ${productName}`);
+  }
+
   return (
     <>
       <h1>Discover Your Favorites</h1>
       <div className="product-list">
         {props.products.map((product) => (
-          <ProductCard key={product.id} {...product} />
+          <ProductCard
+            key={product.id}
+            {...product}
+            onAddToCart={EventAddCart}
+          />
         ))}
       </div>
     </>
